a391b14d7174ccedfb41ca2084bd0dbcf8419090
Mut Ze <mutongsoft@gmail.com>
2023-11-15 15:11:07 +0800

    Add a simple MarshalParser implementation, with only minimal type support

---
+++ src/marshal.test.ts
@@ -0,0 +1,111 @@
+import { MarshalParser } from "./marshal";
+import child, { ChildProcess } from "child_process";
+
+function generate(objects: string[], version: number = 0) {
+    const lines: string[] = [
+        "import sys",
+        "import marshal",
+    ];
+    for (const o of objects) {
+        lines.push(`marshal.dump(${o}, sys.stdout.buffer, ${version})`);
+    }
+    return child.spawn("python3", ["-c", lines.join("\n")], { stdio: ["pipe", "pipe", 2] });
+}
+
+async function parse(process: ChildProcess) {
+    const parser = new MarshalParser(process.stdout!);
+    const items: any[] = [];
+    for await (const item of parser.consume()) {
+        items.push(item);
+    }
+    return items;
+}
+
+jest.setTimeout(60 * 60 * 1000);
+
+describe("literal", () => {
+
+    test("none", async () => {
+        const items = await parse(generate([
+            "None",
+        ]));
+        expect(items).toEqual([
+            null,
+        ]);
+    });
+
+    test("false", async () => {
+        const items = await parse(generate([
+            "False",
+        ]));
+        expect(items).toEqual([
+            false,
+        ]);
+    });
+
+    test("true", async () => {
+        const items = await parse(generate([
+            "True",
+        ]));
+        expect(items).toEqual([
+            true,
+        ]);
+    });
+
+    test("int", async () => {
+        const items = await parse(generate([
+            "100",
+        ]));
+        expect(items).toEqual([
+            100,
+        ]);
+    });
+
+    test("float", async () => {
+        const items = await parse(generate([
+            "0.0009765625",
+        ]));
+        expect(items).toEqual([
+            0.0009765625,
+        ]);
+    });
+
+    test("binary float", async () => {
+        const items = await parse(generate([
+            "0.0009765625",
+        ], 2));
+        expect(items).toEqual([
+            0.0009765625,
+        ]);
+    });
+
+    test("string", async () => {
+        const items = await parse(generate([
+            "b'abcd'",
+        ]));
+        expect(items).toEqual([
+            "abcd",
+        ]);
+    });
+
+    test("dict", async () => {
+        const items = await parse(generate([
+            "{1:2,3:4}",
+        ]));
+        expect(items).toEqual([
+            new Map([[1, 2], [3, 4]]),
+        ]);
+    });
+
+    test("multiple streams", async () => {
+        const items = await parse(generate([
+            "{1:2,3:4}",
+            "{b'1':b'2',b'3':b'4'}",
+        ]));
+        expect(items).toEqual([
+            new Map([[1, 2], [3, 4]]),
+            new Map([["1", "2"], ["3", "4"]]),
+        ]);
+    });
+
+});
---
+++ src/marshal.ts
@@ -0,0 +1,281 @@
+import { Readable } from "stream";
+import Buffers from "./buffers";
+
+enum DataType {
+    Null = 0x30, // '0'
+    None = 0x4e, // 'N'
+    False = 0x46, // 'F'
+    True = 0x54, // 'T'
+    StopIter = 0x53, // 'S'
+    Ellipsis = 0x2e, // '.'
+    Int = 0x69, // 'i'
+    Int64 = 0x49, // 'I'
+    Float = 0x66, // 'f'
+    BinaryFloat = 0x67, // 'g'
+    Complex = 0x78, // 'x
+    BinaryComplex = 0x79, // 'y
+    Long = 0x6c, // 'l'
+    String = 0x73, // 's'
+    Interned = 0x74, // 't'
+    Stringref = 0x52, // 'R'
+    Tuple = 0x28, // '('
+    List = 0x5b, // '['
+    Dict = 0x7b, // '{'
+    Code = 0x63, // 'c'
+    Unicode = 0x75, // 'u'
+    Unknown = 0x3f, // '?'
+    Set = 0x3c, // '<'
+    Frozenset = 0x3e, // '>'
+}
+
+interface Pointer {
+    type: string
+    dirty: boolean
+    dataType?: DataType
+    obj?: any
+    objKey?: any
+}
+
+export class MarshalParser {
+
+    stream: Readable;
+    buffers: Buffers;
+    root: unknown;
+    ptrs: Array<Pointer>;
+
+    constructor(stream: Readable) {
+        this.stream = stream;
+        this.buffers = new Buffers();
+        this.root = undefined;
+        this.ptrs = [];
+    }
+
+    ptr(dirty: boolean = false) {
+        if (this.ptrs.length == 0) {
+            return null;
+        }
+        const ptr = this.ptrs[this.ptrs.length - 1];
+        if (dirty) {
+            ptr.dirty = true;
+        }
+        return ptr;
+    }
+
+    parseType(): boolean {
+        const type_buf = this.buffers.consume(1);
+        if (type_buf === null) {
+            return false;
+        }
+        const type = type_buf[0];
+        if (DataType[type] === undefined) {
+            throw new Error(`unrecognized type: ${type}`);
+        }
+        this.ptrs[this.ptrs.length - 1].dataType = type;
+        return true;
+    }
+
+    setValue(value: unknown) {
+        const ptr = this.ptr(true)!;
+        switch (ptr.type) {
+        case "root":
+            this.root = value;
+            this.ptrs.pop();
+            break;
+        case "dict":
+            if (ptr.objKey === undefined) {
+                ptr.objKey = value;
+            } else {
+                ptr.obj.set(ptr.objKey, value);
+                ptr.objKey = undefined;
+            }
+            ptr.dataType = undefined;
+            break;
+        default:
+            throw new Error("should never reach this");
+        }
+    }
+
+    handleNull(): boolean {
+        const ptr = this.ptr(true)!;
+        switch (ptr.type) {
+        case "root":
+            throw new Error("encountered null in root object");
+        case "dict":
+            if (ptr.objKey !== undefined) {
+                throw new Error("incomplete dict");
+            }
+            this.ptrs.pop();
+            this.ptrs.pop();
+        }
+        return true;
+    }
+
+    handleNone(): boolean {
+        this.setValue(null);
+        return true;
+    }
+
+    handleFalse(): boolean {
+        this.setValue(false);
+        return true;
+    }
+
+    handleTrue(): boolean {
+        this.setValue(true);
+        return true;
+    }
+
+    handleInt(): boolean {
+        const buf = this.buffers.consume(4);
+        if (buf == null) {
+            return false;
+        }
+        this.setValue(buf.readInt32LE());
+        return true;
+    }
+
+    handleInt64(): boolean {
+        const buf = this.buffers.consume(8);
+        if (buf == null) {
+            return false;
+        }
+        this.setValue(buf.readBigInt64LE());
+        return true;
+    }
+
+    handleFloat(): boolean {
+        const sizeBuf = this.buffers.peek(1);
+        if (sizeBuf == null) {
+            return false;
+        }
+        const size = sizeBuf[0];
+        const floatTextBuf = this.buffers.consume(1 + size);
+        if (floatTextBuf == null) {
+            return false;
+        }
+        const floatText = floatTextBuf.toString("utf8", 1);
+        this.setValue(parseFloat(floatText));
+        return true;
+    }
+
+    handleBinaryFloat(): boolean {
+        const buf = this.buffers.consume(8);
+        if (buf == null) {
+            return false;
+        }
+        this.setValue(buf.readDoubleLE());
+        return true;
+    }
+
+    handleString(): boolean {
+        const sizeBuf = this.buffers.peek(4);
+        if (sizeBuf == null) {
+            return false;
+        }
+        const size = sizeBuf.readInt32LE();
+        const stringBuf = this.buffers.consume(4 + size);
+        if (stringBuf == null) {
+            return false;
+        }
+        this.setValue(stringBuf.toString("utf8", 4));
+        return true;
+    }
+
+    handleDict(): boolean {
+        const ptr = this.ptr(true)!;
+        switch (ptr.type) {
+        case "root":
+            this.root = new Map();
+            this.ptrs.push({
+                "type": "dict",
+                "dirty": false,
+                "obj": this.root,
+                "objKey": undefined
+            });
+            break;
+        default:
+            throw new Error("should never reach this");
+        }
+        return true;
+    }
+
+    parse(): boolean {
+        const ptr = this.ptr();
+        if (ptr == null) {
+            throw new Error("no value slots");
+        }
+        if (ptr.dataType === undefined) {
+            return this.parseType();
+        }
+        switch (ptr.dataType) {
+        case DataType.Null:
+            return this.handleNull();
+        case DataType.None:
+            return this.handleNone();
+        case DataType.False:
+            return this.handleFalse();
+        case DataType.True:
+            return this.handleTrue();
+        case DataType.StopIter:
+            throw new Error("StopIter is not supported");
+        case DataType.Ellipsis:
+            throw new Error("Ellipsis is not supported");
+        case DataType.Int:
+            return this.handleInt();
+        case DataType.Int64:
+            return this.handleInt64();
+        case DataType.Float:
+            return this.handleFloat();
+        case DataType.BinaryFloat:
+            return this.handleBinaryFloat();
+        case DataType.Complex:
+            throw new Error("Complex is not supported");
+        case DataType.BinaryComplex:
+            throw new Error("BinaryComplex is not supported");
+        case DataType.Long:
+            throw new Error("Long is not supported");
+        case DataType.String:
+            return this.handleString();
+        case DataType.Interned:
+            throw new Error("Interned is not supported");
+        case DataType.Stringref:
+            throw new Error("Stringref is not supported");
+        case DataType.Tuple:
+            throw new Error("Tuple is not supported");
+        case DataType.List:
+            throw new Error("List is not supported");
+        case DataType.Dict:
+            return this.handleDict();
+        case DataType.Code:
+            throw new Error("Code is not supported");
+        case DataType.Unicode:
+            throw new Error("Unicode is not supported");
+        case DataType.Unknown:
+            throw new Error("Unknown is not supported");
+        case DataType.Set:
+            throw new Error("Set is not supported");
+        case DataType.Frozenset:
+            throw new Error("Frozenset is not supported");
+        default:
+            throw new Error(`unrecognized type: ${ptr.dataType}`);
+        }
+    }
+
+    async * consume() {
+        this.ptrs.push({ "type": "root", "dirty": false });
+        for await (const chunk of this.stream) {
+            this.buffers.push(chunk);
+            while (this.parse()) {
+                if (this.ptrs.length == 0) {
+                    yield this.root;
+                    this.root = undefined;
+                    this.ptrs.push({ "type": "root", "dirty": false });
+                }
+            }
+        }
+        if (this.ptrs[0].dirty) {
+            throw new Error("incomplete stream");
+        }
+    }
+
+}
