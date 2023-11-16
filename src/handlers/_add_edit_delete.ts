5d3d6a4ed5ad9dd9de2026e8230e5f8c7881ada9
Mut Ze <mutongsoft@gmail.com>
2023-11-16 21:31:09 +0800

    Make all log print in finalize

--- src/handlers/_add_edit_delete.ts
+++ src/handlers/_add_edit_delete.ts
@@ -2,24 +2,30 @@ import Handler from "./base";
 import { ErrorMessage, FileActionMessage, InfoMessage, StatMessage } from "./types";
 import { actionConvert } from "../convert";
 
+interface FileAction extends FileActionMessage {
+    messages: string[];
+}
+
 export default class AddEditDeleteHandler extends Handler {
 
-    currentFile: string | null = null;
+    currentAction: FileAction | null = null;
+    actions: FileAction[] = [];
+    messages: InfoMessage[] = [];
 
     stat(stat: StatMessage) {
-        const fa = stat as FileActionMessage;
-        this.currentFile = fa.depotFile;
-        console.log(`[${actionConvert.short[fa.action]}] ${fa.depotFile}`);
+        const action: FileAction = { ...stat as FileActionMessage, messages: [] };
+        this.actions.push(action);
+        this.currentAction = action;
     }
 
     info(info: InfoMessage) {
         const lastDash = info.data.lastIndexOf(" - ");
         const fileName = info.data.substring(0, lastDash);
-        if (fileName == this.currentFile) {
-            const infoMessage = info.data.substring(lastDash + 3).trim();
-            console.log(`    ! ${infoMessage}`);
+        if (fileName == this.currentAction?.depotFile) {
+            const infoText = info.data.substring(lastDash + 3).trim();
+            this.currentAction.messages.push(infoText);
         } else {
-            console.log(`${info.data.trim()}`);
+            this.messages.push(info);
         }
     }
 
@@ -28,7 +34,19 @@ export default class AddEditDeleteHandler extends Handler {
     }
 
     async finalize() {
-
+        if (this.option.root) {
+            for (const action of this.actions) {
+                const color = actionConvert.color[action.action];
+                console.log(color(`[${actionConvert.short[action.action]}] ${action.depotFile}`));
+                for (const message of action.messages) {
+                    console.log(`  - ${message}`);
+                }
+            }
+            for (const message of this.messages) {
+                console.log(message.data.trim());
+            }
+        }
+        return this.actions;
     }
 
 }
