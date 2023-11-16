fe52cea3766638ba2bfd039474df5f8f23a77135
Mut Ze <mutongsoft@gmail.com>
2023-11-16 21:30:07 +0800

    Only print data on root; and print info messages

--- src/handlers/_status.ts
+++ src/handlers/_status.ts
@@ -51,34 +51,40 @@ export default class StatusHandler extends Handler {
     }
 
     async finalize() {
-        const changesDetail = await Promise.all(this.descriptionPromises);
-        for (const detail of changesDetail) {
-            if (!detail) {
-                throw new Error("p4 change returned nothing");
+        if (this.option.root) {
+            const changesDetail = await Promise.all(this.descriptionPromises);
+            for (const detail of changesDetail) {
+                if (!detail) {
+                    throw new Error("p4 change returned nothing");
+                }
+                const change = this.changes[detail.Change];
+                if (change === undefined) {
+                    throw new Error("p4 change returned non-exist changelist");
+                }
+                change.description = detail.Description.trim();
             }
-            const change = this.changes[detail.Change];
-            if (change === undefined) {
-                throw new Error("p4 change returned non-exist changelist");
+            for (const [name, change] of Object.entries(this.changes)) {
+                if (name == "") {
+                    console.log("Untracked files:");
+                    console.log("  (use p4 add/edit/delete/reconcile to track them)");
+                } else if (name == "default") {
+                    console.log("Changelist default:");
+                    console.log("  (use p4 reopen to move them to numbered changelist)");
+                } else if (change) {
+                    console.log(`Changelist #${change.name}: ${change.description}`);
+                    console.log("  (use p4 shelve to push them to server)");
+                }
+                for (const file of change.files) {
+                    const color = actionConvert.color[file.action];
+                    console.log(color(`\t[${actionConvert.short[file.action]}] ${file.depotFile}`));
+                }
+                console.log();
             }
-            change.description = detail.Description.trim();
+            for (const message of this.messages) {
+                console.log(message.data.trim());
+            }
         }
-        for (const [name, change] of Object.entries(this.changes)) {
-            if (name == "") {
-                console.log("Untracked files:");
-                console.log("  (use p4 add/edit/delete/reconcile to track them)");
-            } else if (name == "default") {
-                console.log("Changelist default:");
-                console.log("  (use p4 reopen to move them to numbered changelist)");
-            } else if (change) {
-                console.log(`Changelist #${change.name}: ${change.description}`);
-                console.log("  (use p4 shelve to push them to server)");
-            }
-            for (const file of change.files) {
-                const color = actionConvert.color[file.action];
-                console.log(color(`\t[${actionConvert.short[file.action]}] ${file.depotFile}`));
-            }
-            console.log();
-        }
+        return this.changes;
     }
 
 }
