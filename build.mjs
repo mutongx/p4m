369cad6106a0781db4ccb67e8756a678db93a771
Mut Ze <mutongsoft@gmail.com>
2023-11-20 11:17:15 +0800

    Put all build process from tspkg.yaml to build script

--- build.mjs
+++ build.mjs
@@ -1,3 +1,7 @@
+import child from "child_process";
+import process from "process";
+import { copyFile, readFile } from "fs/promises";
+import { inject } from "postject";
 import * as esbuild from "esbuild";
 
 await esbuild.build({
@@ -8,3 +12,8 @@ await esbuild.build({
     format: "cjs",
     banner: { "js": "#!/usr/bin/env node" },
 });
+
+await new Promise((resolve) => { child.spawn(process.execPath, ["--experimental-sea-config", "sea-config.json"], { stdio: "inherit" }).on("exit", resolve); })
+    .then(() => copyFile(process.execPath, "dist/p4m"))
+    .then(() => readFile("dist/sea-prep.blob"))
+    .then((buffer) => inject("dist/p4m", "NODE_SEA_BLOB", buffer, { sentinelFuse: "NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2" }));
