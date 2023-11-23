eb3cd146a5d959e1faee221e2e1215560956e521
Mut Ze <mutongsoft@gmail.com>
2023-11-23 22:45:16 +0800

    Convert map to object according to object spec

---
+++ src/handlers/p4object.test.ts
@@ -0,0 +1,87 @@
+import { parse } from "./p4object";
+
+describe("ObjectSpect parse", () => {
+    test("parse literal field", () => {
+        const SimpleObjectSpec = {
+            "stringField": ["required", "string"],
+            "numberField": ["optional", "number"],
+        } as const;
+        const data = new Map<string, unknown>([
+            ["stringField", "abcde"],
+            ["numberField", 100],
+        ]);
+        expect(parse(SimpleObjectSpec, data)).toEqual({
+            "stringField": "abcde",
+            "numberField": 100,
+        });
+
+    });
+
+    test("parse array field", () => {
+        const ArrayObjectSpec = {
+            "stringArray": ["array", "string"],
+            "numberArray": ["array", "number"],
+        } as const;
+        const data = new Map<string, unknown>([
+            ["stringArray0", "abcd"],
+            ["stringArray1", "efgh"],
+            ["stringArray2", "ijkl"],
+            ["stringArray3", "mnop"],
+            ["stringArray4", "qrst"],
+            ["stringArray5", "uvwx"],
+            ["stringArray6", "yzab"],
+            ["stringArray7", "cdef"],
+            ["stringArray8", "ghij"],
+            ["stringArray9", "klmn"],
+            ["stringArray10", "opqr"],
+        ]);
+        expect(parse(ArrayObjectSpec, data)).toEqual({
+            "stringArray": ["abcd", "efgh", "ijkl", "mnop", "qrst", "uvwx", "yzab", "cdef", "ghij", "klmn", "opqr"],
+            "numberArray": [],
+        });
+    });
+
+    test("parse nested object", () => {
+        const NestedObjectSpec = {
+            "plainField": ["required", "string"],
+            "nestedObject": ["required", {
+                "nestedField": ["required", "number"],
+            }],
+        } as const;
+        const data = new Map<string, unknown>([
+            ["plainField", "abcd"],
+            ["nestedField", "efgh"],
+        ]);
+        expect(parse(NestedObjectSpec, data)).toEqual({
+            "plainField": "abcd",
+            "nestedObject": {
+                "nestedField": "efgh",
+            }
+        });
+    });
+
+    test("parse nested object array", () => {
+        const NestedObjectSpec = {
+            "plainField": ["required", "number"],
+            "nestedObjectArray": ["array", {
+                "nestedField": ["required", "string"],
+                "anotherNestedField": ["required", "number"],
+            }],
+        } as const;
+        const data = new Map<string, unknown>([
+            ["plainField", 1000],
+            ["nestedField0", "efgh"],
+            ["anotherNestedField0", 1111],
+            ["nestedField1", "ijkl"],
+            ["anotherNestedField1", 2222],
+        ]);
+        expect(parse(NestedObjectSpec, data)).toEqual({
+            "plainField": 1000,
+            "nestedObjectArray": [
+                { "nestedField": "efgh", "anotherNestedField": 1111 },
+                { "nestedField": "ijkl", "anotherNestedField": 2222 },
+            ]
+        });
+    });
+
+});
--- src/handlers/p4object.ts
+++ src/handlers/p4object.ts
@@ -41,7 +41,8 @@ export const ChangeConfigSpec = {
     "User": ["required", "string"],
     "Status": ["required", "string"],
     "Type": ["required", "string"],
-    "Description": ["required", "string"]
+    "Description": ["required", "string"],
+    "Files": ["array", "string"],
 } as const;
 
 export const ShelvedFileSpec = {
@@ -49,9 +50,120 @@ export const ShelvedFileSpec = {
     "openFiles": ["optional", "string"],
     "depotFile": ["required", "string"],
     "rev": ["required", "string"],
-    "action": ["required", "string"]
+    "action": ["required", "string"],
 } as const;
 
 export function parse<T extends ObjectSpec>(spec: T, message: Map<string, unknown>): P4Object<T> {
-    return Object.fromEntries(message) as unknown as P4Object<T>;
+    const result: unknown = {};
+    const prefixes: Record<string, string[]> = {};
+
+    function initializeFromSpec(spec: ObjectSpec, prefix: string[], inArray: boolean) {
+        function assign(prefix: string[], key: string, value: unknown) {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            let obj = result as any;
+            for (const p of prefix) {
+                obj = obj[p];
+            }
+            obj[key] = value;
+        }
+        for (const [key, [storage, type]] of Object.entries(spec)) {
+            let inArrayNext = inArray;
+            if (storage == "array") {
+                // TODO: Move this check to type definition
+                if (inArrayNext) {
+                    throw new Error("nested array is not supported");
+                }
+                assign(prefix, key, []);
+                inArrayNext = true;
+            }
+            if (typeof type == "string") {   
+                if (!inArrayNext) {
+                    assign(prefix, key, undefined);
+                }
+                prefixes[key] = prefix;
+            } else {
+                // TODO: Move this check to type definition
+                if (inArray) {
+                    throw new Error("nested object in array is not supported");
+                }
+                if (!inArrayNext) {
+                    assign(prefix, key, {});
+                }
+                initializeFromSpec(type, [...prefix, key], inArrayNext);
+            }
+        }
+    }
+
+    function parseArrayFieldKey(key: string): [string, number | null] {
+        let numberCount = 0;
+        while (numberCount < key.length) {
+            const code = key.charCodeAt(key.length - 1 - numberCount);
+            if (code < 48 || code > 57) {
+                break;
+            }
+            numberCount++;
+        }
+        if (numberCount > 0) {
+            const realKey = key.substring(0, key.length - numberCount);
+            if (Object.hasOwn(prefixes, realKey)) {
+                return [
+                    key.substring(0, key.length - numberCount),
+                    parseInt(key.substring(key.length - numberCount))
+                ];
+            }
+        }
+        return [key, null];
+    }
+
+    function fillObject(prefix: string[], key: string, index: number | null, value: unknown) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        let obj = result as any;
+        for (const item of prefix) {
+            if (obj[item] === undefined) {
+                throw new Error(`unexpected object field: ${item}`);
+            }
+            if (Array.isArray(obj[item])) {
+                if (index === null) {
+                    throw new Error(`unexpected array field: ${item}`);
+                }
+                if (index < obj[item].length) {
+                    obj = obj[item][index];
+                } else if (index == obj[item].length) {
+                    const newObj = {};
+                    obj[item].push(newObj);
+                    obj = newObj;
+                } else {
+                    throw new Error(`size of array exceeded: ${item}`);
+                }
+            } else {
+                obj = obj[item];
+            }
+        }
+        if (Array.isArray(obj[key])) {
+            if (index === null) {
+                // TODO: For fstat command, we have both otherOpen array and otherOpen field. This will fail.
+                throw new Error(`unexpected array field: ${key}`);
+            }
+            if (index != obj[key].length) {
+                throw new Error(`unexpected array index: ${key}[${index}]`);
+            }
+            obj[key].push(value);
+        } else {
+            obj[key] = value;
+        }
+    }
+
+    initializeFromSpec(spec, [], false);
+
+    for (const field of message.keys()) {
+        const value = message.get(field);
+        const [key, index] = parseArrayFieldKey(field);
+        if (!Object.hasOwn(prefixes, key)) {
+            // TODO: Notify the user about unrecognized field
+            continue;
+        }
+        fillObject(prefixes[key], key, index, value);
+    }
+
+    return result as P4Object<T>;
 }
