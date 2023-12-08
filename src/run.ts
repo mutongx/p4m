167057c83dcef7314656cd2566dc4ce8c5757645
Mut Ze <mutongsoft@gmail.com>
2023-12-08 10:52:10 +0800

    Move editor.ts to run.ts

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,7 @@
 import { spawn } from "child_process";
 import { open } from "fs/promises";
+import { Texts } from "./consts";
 import { MarshalParser } from "./marshal";
-import { generateVimArgs } from "./editor";
 import Handler from "./handlers/base";
 
 export async function run<T>(command: string, args: string[], handler: Handler<T>): Promise<T> {
@@ -42,6 +42,27 @@ export async function runPassthrough(args: string[]) {
 }
 
 export async function runEditor(args: string[]) {
+    async function generateVimArgs(args: string[]): Promise<string[]> {
+        if (args.length != 1) {
+            return [];
+        }
+        let enterLine: number = 0;
+        let enterFound: boolean = false;
+        const file = await open(args[0]);
+        for await (const line of file.readLines()) {
+            enterLine += 1;
+            if (line === `\t${Texts.descriptionPlaceholder}`) {
+                enterFound = true;
+                break;
+            }
+        }
+        await file.close();
+        if (enterFound) {
+            return [`+${enterLine}`, "-c", `s/${Texts.descriptionPlaceholder}//`, "-c", "startinsert"];
+        } else {
+            return [];
+        }
+    }
     args.shift();
     const vimArgs = await generateVimArgs(args);
     const tty = await open("/dev/tty", "w+");
